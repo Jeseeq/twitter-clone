69d53f666e500100f0e5af2e31ac333ea6cf9688
Vitalii <v.hrytsiienko@gmail.com>
2016-07-17 15:47:04 +0300

    Initial commit

---
+++ devServer.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.dev');
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+const app = express();
+const compiler = webpack(webpackConfig);
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
+
+if (isDeveloping) {
+  const compiler = webpack(webpackConfig);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+} else {
+  app.use('/static', express.static(path.join(__dirname, 'dist' )));
+
+}
+
+// app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
---
+++ webpack.config.dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'eventsource-polyfill', 
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        exclude: [/node_modules/],
+        loader: ['babel'],
+        query: {
+          "presets": ["react", "es2015", "stage-0", "react-hmre"]
+        }
+        // include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+      
+    ]
+  }
+};
